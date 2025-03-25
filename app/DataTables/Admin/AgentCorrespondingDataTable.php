34b484019e4be4aab90e0bc7c89b2512e413a88e
sreejith <srkr335@gmail.com>
2025-03-25 13:42:44 +0530

    agent-update

--- app/DataTables/Admin/AgentCorrespondingDataTable.php
+++ app/DataTables/Admin/AgentCorrespondingDataTable.php
@@ -6,6 +6,8 @@ use App\Lead;
 use App\DataTables\BaseDataTable;
 use Yajra\DataTables\Html\Button;
 use Yajra\DataTables\Html\Column;
+use Carbon\Carbon;
+
 
 class AgentCorrespondingDataTable extends BaseDataTable
 {
@@ -15,21 +17,36 @@ class AgentCorrespondingDataTable extends BaseDataTable
      * @param mixed $query Results from query() method.
      * @return \Yajra\DataTables\DataTableAbstract
      */
-    public function dataTable($query)
-    {
-        return datatables()->eloquent($query)
-            ->addIndexColumn()
-            ->addColumn('action', function ($row) {
-                return '<a href="/admin/leads/edit/'.$row->id.'" class="btn btn-primary btn-sm">Edit</a>';
-            })
-            ->editColumn('call_status', function ($row) {
-                return $row->call_status == 1
-                    ? '<span class="badge badge-success">Called</span>'
-                    : '<span class="badge badge-danger">Not Called</span>';
-            })
-            ->rawColumns(['action', 'call_status']);
-    }
-    
+   public function dataTable($query)
+{
+    return datatables()->eloquent($query)
+        ->addIndexColumn()
+        ->editColumn('call_status', function ($row) {
+            return $row->call_status == 1
+                ? '<span class="badge badge-success" style="display: inline-block; width: 100%; text-align: center;">Called</span>'
+                : '<span class="badge badge-danger" style="display: inline-block; width: 100%; text-align: center;">Not Called</span>';
+        })
+
+        ->editColumn('next_follow_up', function ($row) {
+            return $row->next_follow_up === 'yes'
+               ? '<span class="badge badge-success" style="display: inline-block; width: 100%; text-align: center;">Yes</span>'
+        : '<span class="badge badge-danger" style="display: inline-block; width: 100%; text-align: center;">No</span>';
+        })
+
+       ->editColumn('next_follow_up_date', function ($row) {
+    return !empty($row->next_follow_up_date)
+        ? \Carbon\Carbon::parse($row->next_follow_up_date)->format('F d, Y')
+        : '--';
+})
+
+        ->editColumn('remark', function ($row) {
+            return $row->next_follow_up === 'yes'
+                ? $row->remark ?? '--'
+                : '--';
+        })
+        ->rawColumns(['next_follow_up','next_follow_up_date','remark', 'call_status']);
+}
+
 
 
     /**
@@ -44,19 +61,27 @@ class AgentCorrespondingDataTable extends BaseDataTable
 public function query(Lead $model)
 {
     $id = $this->id; // Access the passed ID using with()
+
     return $model->join('lead_agents', 'lead_agents.id', '=', 'leads.agent_id')
         ->join('companies', 'lead_agents.company_id', '=', 'companies.id')
+        ->leftJoin('lead_follow_up', function ($join) {
+            $join->on('lead_follow_up.lead_id', '=', 'leads.id')
+                 ->where('lead_follow_up.is_hidden', 0)
+                 ->orderBy('lead_follow_up.next_follow_up_date', 'asc');
+        })
         ->select(
             'leads.id',
             'leads.client_name',
             'companies.company_name',
-            'leads.value',
+            // 'leads.value',
             'leads.next_follow_up',
-            'leads.call_status'
+            'leads.call_status',
+            'lead_follow_up.next_follow_up_date',
+            'lead_follow_up.remark'
         )
         ->where('leads.agent_id', $id);
 }
-    
+   
     /**
      * Optional method if you want to use html builder.
      *
@@ -100,17 +125,18 @@ public function query(Lead $model)
 
     
 
-    protected function getColumns()
-    {
-        return [
-            ['data' => 'client_name', 'title' => 'Client Name'],
-            ['data' => 'company_name', 'title' => 'Company Name'],
-            ['data' => 'value', 'title' => 'Lead Value'],
-            ['data' => 'next_follow_up', 'title' => 'Next Follow-up'],
-            ['data' => 'call_status', 'title' => 'Call Status'],
-            ['data' => 'action', 'title' => 'Action', 'orderable' => false, 'searchable' => false],
-        ];
-    }
+   protected function getColumns()
+{
+    return [
+        ['data' => 'client_name', 'title' => 'Client Name'],
+        ['data' => 'company_name', 'title' => 'Company Name'],
+        // ['data' => 'value', 'title' => 'Lead Value'],
+        ['data' => 'next_follow_up', 'title' => 'Next Follow-up'],
+        ['data' => 'next_follow_up_date', 'title' => 'Next Follow-up Date'],
+        ['data' => 'remark', 'title' => 'Remark'],
+        ['data' => 'call_status', 'title' => 'Call Status'],
+    ];
+}
 
 
 
